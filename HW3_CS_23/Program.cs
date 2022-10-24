ad60161491a17b8d9432a1d42f2277fa8eebbef2
Denis <help@denis-getcourse.ru>
2022-10-24 21:20:01 +0300

    HW3_CS_23

---
+++ HW3_CS_23/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int[] getCubes(int N)
+{
+    int[] arr = new int[N];
+    for (int i = 1; i <= N; i++)
+    {
+        arr[i - 1] = i * i * i;
+    }
+    return arr;
+}
+
+void printArray(int[] arr, int N)
+{
+    string str = "Ответ: ";
+    for (int i = 0; i < N; i++)
+    {
+        if (i == N - 1)
+        {
+            str += arr[i] + ". ";
+        }
+        else
+        {
+            str += arr[i] + ", ";
+        }
+    }
+    Console.WriteLine(str);
+}
+
+int N = GetNumber();
+
+int[] arr = getCubes(N);
+
+printArray(arr, N);
