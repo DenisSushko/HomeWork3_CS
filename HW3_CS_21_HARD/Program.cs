32be2efc2e4a06fa0bc815d54f14be5a66ef2647
Denis <help@denis-getcourse.ru>
2022-10-24 21:23:28 +0300

    HW3_CS_21_HARD

---
+++ HW3_CS_21_HARD/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 21 - HARD необязательная
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве.
+Сначала задается N с клавиатуры, потом задаются координаты точек.*/
+
+int GetN()
+{
+    Console.WriteLine("Enter dimension: ");
+    int N = int.Parse(Console.ReadLine()!);
+    return N;
+}
+
+int[] GetCoords(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return arr;
+}
+
+double GetDistance(int[] arrA, int[] arrB)
+{
+    int sum = 0;
+    for (int i = 0; i < arrA.Length; i++)
+    {
+        sum += (arrB[i] - arrA[i]) * (arrB[i] - arrA[i]);
+    }
+    double dist = Math.Sqrt(sum);
+    return Math.Round(dist, 3);
+
+}
+
+int N = GetN();
+int[] arrayA = new int[N];
+Console.WriteLine("Enter coords for point A: ");
+arrayA = GetCoords(arrayA);
+int[] arrayB = new int[N];
+Console.WriteLine("Enter coords for point B: ");
+arrayB = GetCoords(arrayB);
+double distance = GetDistance(arrayA, arrayB);
+Console.WriteLine($"Distance between points: {distance}");
